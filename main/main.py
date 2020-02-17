3430c4fec103d9b7f8c038b90670684f7ea20555
gitkamhub <brkamleshg678@gmail.com>
2020-02-17 11:36:35 +0530

    changed for loop

--- main/main.py
+++ main/main.py
@@ -6,16 +6,15 @@ def download_and_install_update_if_available():
     ota_updater.download_and_install_update_if_available('kritsnam', 'hydro123')
 
 def start():
-    # your custom code goes here. Something like this: ...
-    # from main.x import YourProject
-    # project = YourProject()
-    # ...
     led = machine.Pin(5, machine.Pin.OUT)
-    for i in range 10:
+    i=0
+    while (i<4):
+        print("running")
         led.value(1)
-	time.sleep(1)
-	led.value(0)
-	time.sleep(1)
+        time.sleep(1)
+        led.value(0)
+        time.sleep(1)
+        i+=1
 
 def boot():
     download_and_install_update_if_available()
